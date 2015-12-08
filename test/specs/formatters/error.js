7647dcbcc1db9e4c94e9c886f1d51802b4386443
Yvem <jutard@lemonde.fr>
2015-12-08 11:50:57 +0100

    no more non-standard property (BREAKING) + better duck typing + better tests

--- test/specs/formatters/error.js
+++ test/specs/formatters/error.js
@@ -5,107 +5,130 @@ var express = require('../../utils/express');
 
 describe('Error formatter', function () {
 
-  context('given string as error', function () {
-    it('should define error message', function (done) {
+  context('given a string as error', function () {
+    it('should define error fields', function (done) {
+      var meta;
       var server = express.createServer(function (req, res, next) {
-        var meta = formatter().format('A string');
-        expect(meta).to.have.deep.property('error.message', 'A string');
-        expect(meta).to.have.deep.property('error.stack', null);
-        expect(meta).to.have.deep.property('error.code', null);
+        meta = formatter().format('A string');
         next();
       });
 
       request(server)
         .get('/test')
         .expect(200)
-        .end(done);
+        .end(function(err) {
+          if (err) return done(err);
+
+          expect(meta).to.have.deep.property('error.name', 'Error');
+          expect(meta).to.have.deep.property('error.message', 'A string');
+          expect(meta).to.have.deep.property('error.stack', null);
+          done();
+        });
     });
   });
 
-  describe('given plain object as error', function () {
-    it('should define error message', function (done) {
+  describe('given a plain object as error', function () {
+    it('should define error fields', function (done) {
+      var meta;
       var server = express.createServer(function (req, res, next) {
-        var meta = formatter().format({foo: 'bar'});
-        expect(meta).to.have.deep.property('error.message', '[object Object]');
-        expect(meta).to.have.deep.property('error.stack', null);
-        expect(meta).to.have.deep.property('error.code', null);
+        meta = formatter().format({foo: 'bar'});
         next();
       });
 
       request(server)
         .get('/test')
         .expect(200)
-        .end(done);
+        .end(function(err) {
+          if (err) return done(err);
+
+          expect(meta).to.have.deep.property('error.name', 'Error');
+          expect(meta).to.have.deep.property('error.message', '[object Object]');
+          expect(meta).to.have.deep.property('error.stack', null);
+          done();
+        });
     });
   });
 
-  describe('given Error object as error', function () {
-
-    it('should define error message', function (done) {
+  describe('given an Error object as error', function () {
+    it('should define error fields', function (done) {
+      var meta;
       var server = express.createServer(function (req, res, next) {
         var error = new Error('Test message');
-
-        var meta = formatter().format(error);
-        expect(meta).to.have.deep.property('error.message', 'Test message');
+        meta = formatter().format(error);
         next();
       });
 
       request(server)
         .get('/test')
         .expect(200)
-        .end(done);
+        .end(function(err) {
+          if (err) return done(err);
+
+          expect(meta).to.have.deep.property('error.message', 'Test message');
+          done();
+        });
     });
 
     context('with default configuration', function () {
-
       it('should pick only selected properties', function (done) {
-      var server = express.createServer(function (req, res, next) {
-        var error = new Error('Test message');
-        error.code = 10;
-        error.foo = 'bar';
+        var meta, error;
 
-        var meta = formatter().format(error);
-        expect(meta).to.have.deep.property('error.message', 'Test message');
-        expect(meta).to.have.deep.property('error.stack', error.stack);
-        expect(meta).to.have.deep.property('error.code', 10);
-        expect(meta).to.not.have.deep.property('error.foo'); // not picked
-        next();
-      });
-
-      request(server)
-        .get('/test')
-        .expect(200)
-        .end(done);
-      });
-    });
-
-    context('with an explicit properties whitelist', function () {
-
-      it('should pick only selected properties', function (done) {
         var server = express.createServer(function (req, res, next) {
-          var error = new Error('Test message');
+          error = new Error('Test message');
           error.code = 10;
           error.foo = 'bar';
 
-          var meta = formatter({
-            pickedFields: [
-              'message',
-              'name',
-              'stack',
-              'foo'
-            ]
-          }).format(error);
-          expect(meta).to.have.deep.property('error.message', 'Test message');
-          expect(meta).to.have.deep.property('error.stack', error.stack);
-          expect(meta).to.not.have.deep.property('error.code'); // not picked
-          expect(meta).to.have.deep.property('error.foo', 'bar'); // picked
+          meta = formatter().format(error);
           next();
         });
 
         request(server)
           .get('/test')
           .expect(200)
-          .end(done);
+          .end(function (err) {
+            if (err) return done(err);
+
+            expect(meta).to.have.deep.property('error.name', 'Error');
+            expect(meta).to.have.deep.property('error.message', 'Test message');
+            expect(meta).to.have.deep.property('error.stack', error.stack);
+            expect(meta).to.not.have.deep.property('error.code'); // not picked
+            expect(meta).to.not.have.deep.property('error.foo'); // not picked
+            done();
+          });
+      });
+    });
+
+    context('with an explicit properties whitelist', function () {
+      it('should pick only selected properties', function (done) {
+        var meta, error;
+        var server = express.createServer(function (req, res, next) {
+          error = new Error('Test message');
+          error.code = 10;
+          error.foo = 'bar';
+
+          meta = formatter({
+            pickedFields: [
+              // no "name"
+              'message',
+              'stack',
+              'foo'
+            ]
+          }).format(error);
+          next();
+        });
+
+        request(server)
+          .get('/test')
+          .expect(200)
+          .end(function(err) {
+            if (err) return done(err);
+
+            expect(meta).to.not.have.deep.property('error.name'); // not picked
+            expect(meta).to.have.deep.property('error.message', 'Test message');
+            expect(meta).to.have.deep.property('error.stack', error.stack);
+            expect(meta).to.have.deep.property('error.foo', 'bar'); // picked
+            done();
+          });
       });
     });
 
