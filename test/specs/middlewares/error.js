7dcb3104e1dd0cbbec61a89ed51b48877c824401
Justin Bourrousse <bourrousse@lemonde.fr>
2018-01-19 16:54:58 +0100

    chore(code): rewrite code with es6 syntax

--- test/specs/middlewares/error.js
+++ test/specs/middlewares/error.js
@@ -1,26 +1,27 @@
-var expect = require('chai').use(require('sinon-chai')).expect;
-var sinon = require('sinon');
-var request = require('supertest');
-var errorLogger = require('../../../index').error;
-var express = require('../../utils/express');
+const { expect } = require('chai').use(require('sinon-chai'))
+const sinon = require('sinon')
+const request = require('supertest')
 
-describe('Error middleware', function () {
-  it('should log error', function (done) {
-    var log = sinon.spy();
-    var server = express.createServer(errorLogger({log: log}), {
-      err: new Error('Custom error')
-    });
+const errorLogger = require('../../../index').error
+const express = require('../../utils/express')
+
+describe('Error middleware', () => {
+  it('should log error', done => {
+    const log = sinon.spy()
+    const server = express.createServer(errorLogger({ log }), {
+      err: new Error('Custom error'),
+    })
 
     request(server)
       .get('/test')
       .expect(500)
-      .expect(function () {
-        expect(log).to.be.calledWith('GET 500 /test');
-        expect(log.firstCall.args[1]).to.have.property('category');
-        expect(log.firstCall.args[1]).to.have.property('clientRequest');
-        expect(log.firstCall.args[1]).to.have.property('error');
-        expect(log.firstCall.args[1]).to.have.property('durationMs');
+      .expect(() => {
+        expect(log).to.be.calledWith('GET 500 /test')
+        expect(log.firstCall.args[1]).to.have.property('category')
+        expect(log.firstCall.args[1]).to.have.property('clientRequest')
+        expect(log.firstCall.args[1]).to.have.property('error')
+        expect(log.firstCall.args[1]).to.have.property('durationMs')
       })
-      .end(done);
-  });
-});
+      .end(done)
+  })
+})
